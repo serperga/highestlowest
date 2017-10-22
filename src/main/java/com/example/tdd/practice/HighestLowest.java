31a1d13ad9de477910d67ef429539048b27ffa0c
sergio perez gago <serperga@gmail.com>
2017-10-22 15:43:25 +0100

    Implementing execute 1 to 100 loop

--- src/main/java/com/example/tdd/practice/HighestLowest.java
+++ src/main/java/com/example/tdd/practice/HighestLowest.java
@@ -7,13 +7,9 @@ public class HighestLowest {
         String result="";
 
         int[] integerArray = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
-        List<Integer> integerList = new ArrayList<Integer>();
-        for(int i = 0;i < integerArray.length;i++){
-            integerList.add(integerArray[i]);
-        }
 
-        if (integerList.size() == 1){
-            result = integerList.get(0).toString() + " " + integerList.get(0).toString();
+        if (integerArray.length == 1){
+            result = integerArray[0] + " " + integerArray[0];
         }
         return result;
     }
